904c183d47424c28760710eb18f7b0eae64d3ef7
Dmitriy Melnichenko <tairezzzz@gmail.com>
2016-06-06 12:23:36 +0300

    images directive

--- src/app/components/product/product.module.js
+++ src/app/components/product/product.module.js
@@ -3,6 +3,7 @@ import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 import { ProductUrlsDirective } from './directives/prdoctUrls/productUrls.directive';
 import { ProductTagsDirective } from './directives/prdoctTags/productTags.directive';
+import { ProductImagesDirective } from './directives/prdoctImages/productImages.directive';
 
 
 export let moduleName = 'productModule';
@@ -12,4 +13,5 @@ angular.module(moduleName, [])
 .controller('ProductController', ProductController)
 .service('productService', ProductService)
 .component('productUrls', ProductUrlsDirective)
+.component('productImages', ProductImagesDirective)
 .component('productTags', ProductTagsDirective)
